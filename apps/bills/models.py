1811cbf87726b464f82cf6ecc11a5dcaae344fa1
JiangInk <JiangInk@foxmail.com>
2018-05-03 22:44:58 +0800

    adjust data model

--- apps/bills/models.py
+++ apps/bills/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.category.models import Categorys
 
 # Create your models here.
-# 账目表
 class Bills(models.Model):
+    """
+    账目信息表
+    """
     OUTGO = '0'     # 账目类型.支出
     INCOME = '1'    # 账目类型.收入
     TYPE_CHOICE = (
@@ -10,15 +14,14 @@ class Bills(models.Model):
         (INCOME, 'INCOME'),
     )
 
-    user = models.TextField() # 账目用户ID ForeignKey()
-    bill_type = models.CharField(max_length=1, choices=TYPE_CHOICE, default=OUTGO) # 账目类型
-    category = models.TextField() # 一级分类ID  
-    subcategory = models.TextField() # 二级分类ID
-    amount = models.DecimalField(max_digits=16, decimal_places=2, default=0) # 账目金额
-    record_date = models.DateField() # 记录时间
-    remarks = models.CharField(max_length=140) # 备注 至多140字
-    modify_time = models.DateTimeField(auto_now=True) # 修改时间
-    create_time = models.DateTimeField(auto_now_add=True) # 创建时间
+    user = models.ForeignKey(User, verbose_name='账目所属用户', blank=True, null=True, on_delete=models.CASCADE)
+    bill_type = models.CharField('账目类型', max_length=1, choices=TYPE_CHOICE, default=OUTGO)
+    category = models.ForeignKey(Categorys, verbose_name='明细分类', blank=True, null=True, on_delete=models.CASCADE)
+    amount = models.DecimalField('账目金额', max_digits=16, decimal_places=2, default=0)
+    record_date = models.DateField('记录时间', auto_now=True)
+    remarks = models.CharField('备注信息', max_length=140) # 至多140字
+    modify_time = models.DateTimeField('修改时间', auto_now=True)
+    create_time = models.DateTimeField('创建时间', auto_now_add=True)
 
     class Meta:
         db_table = "bills"
--- apps/categorys/models.py
+++ apps/categorys/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-# 分类表
 class Categorys(models.Model):
+    """
+    账目明细分类表
+    """
     OUTGO = '0'     # 账目类型.支出
     INCOME = '1'    # 账目类型.收入
     TYPE_CHOICE = (
@@ -10,14 +13,13 @@ class Categorys(models.Model):
         (INCOME, 'INCOME'),
     )
 
-    user = models.TextField() # 用户ID ForeignKey()
-    is_default = model.BooleanField() # 是否为初始默认类型 True:默认存在分类 False:用户自定义分类
-    is_parent = model.BooleanField() # 是否为父类 True:父类 False:子类
-    bill_type = models.CharField(max_length=1, choices=TYPE_CHOICE, default=OUTGO) # 账目类型
-    name = models.TextField() # 分类名称 
-    parent_id = models.TextField() # 父级ID
-    modify_time = models.DateTimeField(auto_now=True) # 修改时间
-    create_time = models.DateTimeField(auto_now_add=True) # 创建时间
+    is_default = model.BooleanField('是否默认分类', default=True) # True:默认存在分类 False:用户自定义分类
+    user = models.ForeignKey(User, verbose_name='自定义分类所属用户', blank=True, null=True, on_delete=models.CASCADE)
+    bill_type = models.CharField('账目类型', max_length=1, choices=TYPE_CHOICE, default=OUTGO)
+    name = models.CharField('分类名称', max_length=20, unique=True) 
+    parent = models.ForeignKey('self', verbose_name='父级分类', blank=True, null=True, on_delete=models.CASCADE)
+    modify_time = models.DateTimeField('修改时间', auto_now=True)
+    create_time = models.DateTimeField('创建时间', auto_now_add=True)
 
     class Meta:
         db_table = "categorys"
