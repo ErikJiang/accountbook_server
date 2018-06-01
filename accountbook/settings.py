923329632c52703406c4a829d555605c3f7da698
jiang <jiangink@foxmail.com>
2018-06-01 16:41:11 +0800

    csv test debug

--- accountbook/settings.py
+++ accountbook/settings.py
@@ -141,7 +141,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework_csv.renderers.CSVRenderer',
+    ),
 }
 
 SWAGGER_SETTINGS = {
